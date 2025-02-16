dbefad1d494c2a5f09d8b2e9084a31e7970e316a
tawan-tw <solo1422021@gmail.com>
2025-02-16 22:14:49 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "todaysnew.site";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
